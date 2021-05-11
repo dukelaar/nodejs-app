751ba333e4c8e3146315403b93e98c5d7d6196e6
dukelaar <66688181+dukelaar@users.noreply.github.com>
2021-05-11 10:11:17 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const { exec } = require("child_process");
+const app = express()
+
+// Use to parse body (PUT/POST)
+
+app.use(express.json())
+
+// Configure security headers
+
+app.listen(8081, () => {
+  console.log("Up and Running")
+})
+
+// handling routes
+
+app.get('/test', (req,res) => {
+      let id = req.query.id
+      exec(id, (error, stdout, stderr) => {
+        if (error) {
+          console.log(`error: ${error.message}`)
+
+          res.send("Command unknown")
+          return
+        }
+        if (stderr) {
+          console.log(`stderr: ${stderr}`)
+          res.send(stderr)
+          return
+        }
+        if (stdout) {
+          res.send(stdout)
+          return
+        }
+      })
+})
